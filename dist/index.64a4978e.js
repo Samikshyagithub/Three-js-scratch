944c4da060d2fa87baef732e3c3c426d5bdaafb1
Samikshyagithub <su58071320@gmail.com>
2023-07-21 13:59:07 +0545

    Learned about the planes.Added planes and then made it double sided.Learned about how a material covers up a geometry.Also about the grid helper

--- dist/index.64a4978e.js
+++ dist/index.64a4978e.js
@@ -580,21 +580,34 @@ const renderer = new _three.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const scene = new _three.Scene(); //to create a scene to the viewer
-const camera = new _three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new _three.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 //orbit is defined to set the position of camera so that we can rotate our object with cursor.
 const orbit = new (0, _orbitControlsJs.OrbitControls)(camera, renderer.domElement);
 const axesHelper = new _three.AxesHelper(5); //automatic creates an axis
 scene.add(axesHelper);
 //our camera has only two axes while it should have three that is why we introduce the z position
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
 orbit.update();
 //now we create a box
+//In three js we first need the geometry then the material and finally we cover up the geometry with our material.
 const boxGeometry = new _three.BoxGeometry();
 const boxMaterial = new _three.MeshBasicMaterial({
     color: 0x00ff00
 });
-const box = new _three.Mesh(boxGeometry, boxMaterial);
+const box = new _three.Mesh(boxGeometry, boxMaterial); //fusion of geometry and material (mesh can be cube or character)
 scene.add(box);
+//creating a plane with const passing width and height as parameters
+const planeGeometry = new _three.PlaneGeometry(30, 30); //this is a geometry
+const planeMaterial = new _three.MeshBasicMaterial({
+    color: 0xffffff,
+    side: _three.DoubleSide
+}); //this the cover as in mesh as in shape
+const plane = new _three.Mesh(planeGeometry, planeMaterial); //this is part where we cover our geometry with material
+scene.add(plane); //The plane should be displayed in the scene .
+plane.rotation.x = -0.5 * Math.PI; //we also want the plane to rotate
+//adding a helper
+const gridHelper = new _three.GridHelper(30); //30 is the width or height
+scene.add(gridHelper);
 //we perform 5 radiation rotation on the box
 //box.rotation.x=5;
 //box.rotation.y=5;
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,7 +9,7 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene(); //to create a scene to the viewer
 
 const camera = new THREE.PerspectiveCamera( //camera's perspective of an object
-  75, // camera's perspective is fine between 40 and 80.
+  45, // camera's perspective is fine between 40 and 80.
   window.innerWidth / window.innerHeight,
   0.1, //near and far clipping plane
   1000
@@ -20,14 +20,29 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 const axesHelper = new THREE.AxesHelper(5); //automatic creates an axis
 scene.add(axesHelper);
 //our camera has only two axes while it should have three that is why we introduce the z position
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
 orbit.update();
 
 //now we create a box
+//In three js we first need the geometry then the material and finally we cover up the geometry with our material.
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
+const box = new THREE.Mesh(boxGeometry, boxMaterial); //fusion of geometry and material (mesh can be cube or character)
 scene.add(box);
+
+//creating a plane with const passing width and height as parameters
+const planeGeometry = new THREE.PlaneGeometry(30, 30); //this is a geometry
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide, //since the plane gets diappeared from lower we need to first make it doubleside
+}); //this the cover as in mesh as in shape
+const plane = new THREE.Mesh(planeGeometry, planeMaterial); //this is part where we cover our geometry with material
+scene.add(plane); //The plane should be displayed in the scene .
+plane.rotation.x = -0.5 * Math.PI; //we also want the plane to rotate
+
+//adding a helper
+const gridHelper = new THREE.GridHelper(30); //30 is the width or height
+scene.add(gridHelper);
 
 //we perform 5 radiation rotation on the box
 //box.rotation.x=5;
